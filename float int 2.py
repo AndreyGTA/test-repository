54d1d1584438310358aed06ab80255378e1d0645
AndreyGTA <94783734+AndreyGTA@users.noreply.github.com>
2023-10-22 21:18:45 +0700

    Add files via upload

---
+++ float int 2.py
@@ -0,0 +1,56 @@
+# num1 = input("Введите первое число? \n")
+# num2 = input("Введите второе число? \n")
+# num3 = input("Введите третье число? \n")
+# num1=int(num1)
+# num2=int(num2)
+# num3=int(num3)
+# print(int,num1+num2+num3)
+# num1 = input("Введите первое число? \n")
+# num2 = input("Введите второе число? \n")
+# num1=int(num1)
+# num2=int(num2)
+# print(int,num1+num2/2)
+# num1 = input("ckoлько дoбaвляешь? \n")
+# num2 = input("ckoлько дoбaвляешь? \n")
+# num3 = input("ckoлько дoбaвляешь? \n")
+# num1=int(num1)
+# num2=int(num2)
+# num3=int(num3)
+# print(15000 - (num1+num2+num3))
+# xleb = 32
+# moloko = 100
+# sosicky = 6
+# vpr = input("сколько ты взял молока? \n")
+# vpr2 = input("сколько ты взял сосисок? \n")
+# vpr3 = input("сколько ты взял хлеба? \n")
+# vpr=int(vpr)
+# vpr2=int(vpr2)
+# vpr3=int(vpr3)
+# print((vpr * moloko)+(vpr2 * sosicky)+(vpr3 * xleb))
+# was = 10000
+# vpr = input("сколько ты должен другу? \n")
+# vpr2 = input("сколько ты должен тренеру? \n")
+# vpr=int(vpr)
+# vpr2=int(vpr2)
+# print(was - vpr - vpr2)
+# klassmeets = 6
+# mandarins = 2000000000
+# print("осталось",mandarins % klassmeets ,"каждый получил по " ,mandarins // klassmeets)
+# a = 56
+# b = 93
+# c = 34
+# print(a*2+b*3+c*4)
+# x = 49
+# y = 65
+# print("3x^2+2xy-6y^2=",(3*x*2)+(2*x*y)-(6*y*2))
+soda = 5500
+candy = 2000
+cugarvata = 300
+vpr1 = input("ckoлько человек хотят газировки? \n")
+vpr2 = input("ckoлько человек хотят конфеты? \n") 
+vpr3 = input("ckoлько человек хотят вату? \n")
+vpr1 = int(vpr1)
+vpr2 = int(vpr2)
+vpr3 = int(vpr3)
+print("газировку",soda // vpr1 ,"конфеты",candy // vpr2,"вату",cugarvata // vpr3)
+

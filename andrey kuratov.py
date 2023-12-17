4df1dc9cdf812c03082ec040c1f578369e6597ba
AndreyGTA <94783734+AndreyGTA@users.noreply.github.com>
2023-12-17 21:03:20 +0700

    Add files via upload

---
+++ andrey kuratov.py
@@ -0,0 +1,12 @@
+# login = djfjd
+# Pyautogui  = 5656
+login = str(input("введите логин :"))
+if login == "djfjd":
+    print("логин верный")
+else:
+    print("логин неверный")
+Pyautogui = int(input("введите пароль :"))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         #дима гей                                           
+if Pyautogui == 5656:
+    print("пароль верный")
+else:
+    print("пароль неверный")
